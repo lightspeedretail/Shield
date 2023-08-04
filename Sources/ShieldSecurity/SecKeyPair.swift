83d2af9d25a5a46aca00a75a36e4d5d60aa7b5d0
Philippe Casgrain <philippe.casgrain@lightspeedhq.com>
2023-08-04 11:42:46 -0400

    dont so an extra save with kSecAttrIsPermanent?

--- Sources/ShieldSecurity/SecKeyPair.swift
+++ Sources/ShieldSecurity/SecKeyPair.swift
@@ -68,25 +68,22 @@ public struct SecKeyPair {
       guard let type = type else { fatalError("missing key type") }
       guard let keySize = keySize else { fatalError("missing key size") }
 
-      var privateKeyAttrs: [CFString: Any] = [
-        kSecAttrIsPermanent: true,
-      ]
-
-      var publicKeyAttrs: [CFString: Any] = [
-        kSecAttrIsPermanent: true,
-      ]
-
-      if let tag = tag {
-          privateKeyAttrs[kSecAttrApplicationTag] = tag
-          publicKeyAttrs[kSecAttrApplicationTag] = tag
-      }
-
       var attrs: [CFString: Any] = [
         kSecAttrKeyType: type.systemValue,
         kSecAttrKeySizeInBits: keySize,
-        kSecPrivateKeyAttrs: privateKeyAttrs,
-        kSecPublicKeyAttrs: publicKeyAttrs,
       ]
+
+      if let tag = tag {
+        let privateKeyAttrs: [CFString: Any] = [
+          kSecAttrApplicationTag: tag
+        ]
+        let publicKeyAttrs: [CFString: Any] = [
+          kSecAttrApplicationTag: tag
+        ]
+
+        attrs[kSecPrivateKeyAttrs] = privateKeyAttrs
+        attrs[kSecPublicKeyAttrs] = publicKeyAttrs
+      }
 
       if let label = label {
         attrs[kSecAttrLabel] = label
